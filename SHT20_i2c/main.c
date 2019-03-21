2af1c33dba05a77550850cd2d7da2bb6535d7885
MaxChiCha <you@example.com>
2019-03-21 20:04:56 +0700

    add SHT20(Usart)

--- SHT20_i2c/main.c
+++ SHT20_i2c/main.c
@@ -30,24 +30,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-// #include "SHT20.h"
-// #define USARTx                           USART2
-// #define USARTx_CLK                       RCC_APB1Periph_USART2
-// #define USARTx_APBPERIPHCLOCK            RCC_APB1PeriphClockCmd
-// #define USARTx_IRQn                      USART2_IRQn
-// #define USARTx_IRQHandler                USART2_IRQHandler
 
-// #define USARTx_TX_PIN                    GPIO_Pin_2                
-// #define USARTx_TX_GPIO_PORT              GPIOA                       
-// #define USARTx_TX_GPIO_CLK               RCC_AHBPeriph_GPIOA
-// #define USARTx_TX_SOURCE                 GPIO_PinSource2
-// #define USARTx_TX_AF                     GPIO_AF_USART2
-
-// #define USARTx_RX_PIN                    GPIO_Pin_3                
-// #define USARTx_RX_GPIO_PORT              GPIOA                    
-// #define USARTx_RX_GPIO_CLK               RCC_AHBPeriph_GPIOA
-// #define USARTx_RX_SOURCE                 GPIO_PinSource3
-// #define USARTx_RX_AF                     GPIO_AF_USART2
 /** @addtogroup Template_Project
   * @{
   */
